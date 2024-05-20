11f9d9e8ed31d454d3af60dd915c1b8637fffd78
Paul Orzel <pb.orzel@proton.me>
2024-05-20 15:14:36 +0200

    use features properly

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 mod error;
 mod field;
+
+#[cfg(feature = "tui")]
 mod tui;
 
+#[cfg(feature = "tui")]
 fn main() {
 	tui::run_tui();
 }
+
+#[cfg(feature = "gui")]
+fn main() {
+	todo!("Gui")
+}
+
+#[cfg(all(feature = "tui", feature = "gui"))]
+compile_error!("feature \"tui\" and feature \"gui\" cannot be enabled at the same time");
+
+#[cfg(all(not(feature = "tui"), not(feature = "gui")))]
+compile_error!("Either feature \"tui\" or feature \"gui\" must be enabled");
