29b957d68b04ee9e064f1f9a85dbc714586bf7e8
Paul Orzel <pb.orzel@proton.me>
2024-05-20 15:19:57 +0200

    make compile errors a bit less cluttered

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,24 @@ mod field;
 #[cfg(feature = "tui")]
 mod tui;
 
-#[cfg(feature = "tui")]
+// TODO: Properly handle "tui" and "gui" being enabled at the same time
+
+#[cfg(all(feature = "tui", not(feature = "gui")))]
 fn main() {
 	tui::run_tui();
 }
 
-#[cfg(feature = "gui")]
+#[cfg(all(feature = "gui", not(feature = "tui")))]
 fn main() {
 	todo!("Gui")
 }
 
 #[cfg(all(feature = "tui", feature = "gui"))]
 compile_error!("feature \"tui\" and feature \"gui\" cannot be enabled at the same time");
+#[cfg(all(feature = "tui", feature = "gui"))]
+fn main() {}
 
 #[cfg(all(not(feature = "tui"), not(feature = "gui")))]
 compile_error!("Either feature \"tui\" or feature \"gui\" must be enabled");
+#[cfg(all(not(feature = "tui"), not(feature = "gui")))]
+fn main() {}
