b7b774fe94fd796905df0f6d5389b4fafc0c51f7
Paul Orzel <pb.orzel@proton.me>
2024-05-28 14:42:38 +0200

    cargo fmt

--- src/tui/args.rs
+++ src/tui/args.rs
@@ -7,14 +7,14 @@ pub struct Args {
 	#[arg(short,long,default_value_t = String::from(""))]
 	pub seed: String,
 
-	#[arg(short,default_value_t = 9)]
+	#[arg(short, default_value_t = 9)]
 	pub x: u8,
 
-	#[arg(short,default_value_t = 9)]
+	#[arg(short, default_value_t = 9)]
 	pub y: u8,
 
-	#[arg(short,default_value_t = 10)]
-	pub mines: u16
+	#[arg(short, default_value_t = 10)]
+	pub mines: u16,
 }
 
 pub fn seed_to_u64(seed: &str) -> u64 {
